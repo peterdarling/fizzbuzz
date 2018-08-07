5719821a531232026eb1aef44646232fcb223934
Pete Darling <pete@natter.ai>
2018-08-07 01:15:35 +0100

    Converted readable PHP to unreadable 2 liner

--- php/index.php
+++ php/index.php
@@ -1,10 +1,3 @@
 <?php
-for ($i = 1; $i <= 100; $i++) {
-    $three = $i % 3 === 0;
-    $five = $i % 5 === 0;
-    echo ($three && $five ? "FizzBuzz":
-         ($three ? "Fizz":
-         ($five ? "Buzz":
-         $i)))
-    .(php_sapi_name() === 'cli' ? PHP_EOL : '<br />');
-}
+for ($i = 1; $i<=100; $i++)
+    printf("%s%s%s\n", $i%3&&$i%5?$i.' ':'', $i%3?'':'Fizz', $i%5?'':'Buzz');
