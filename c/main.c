c8fb0cd2b594a55f8f56ae1117f3586236e56a8e
Pete Darling <pete@natter.ai>
2018-08-07 01:34:16 +0100

    Updated port from PHP which is crazy as it based on C

--- c/main.c
+++ c/main.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    for (int i=1; i<=100; i++) {
-        printf("%s%s%s\n", i%3&&i%5?i.' ':'', i%3?'':'Fizz', i%5?'':'Buzz');
+    for (int i = 1; i<=100; i++) {
+        printf("%s%s%s\n", i%3&&i%5?printf("%d ", i):'', i%3?'':'Fizz', i%5?'':'Buzz');
     }
     return 0;
 }
