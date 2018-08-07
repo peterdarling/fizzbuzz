567ea976cc312f58c9a2dfae1db035be27e427f3
Pete Darling <pete@natter.ai>
2018-08-07 15:07:23 +0100

    Space not needed if no fizz or buzz

--- c/main.c
+++ c/main.c
@@ -4,7 +4,7 @@ int main(void)
 {
     char numStr[5];
     for (int i=1; i<=100; i++) {
-        if (i % 3 && i % 5) sprintf(numStr,"%d ", i);
+        if (i % 3 && i % 5) sprintf(numStr,"%d", i);
         else numStr[0] = '\0';
 
         printf("%s%s%s\n", numStr,
--- php/index-readable.php
+++ php/index-readable.php
@@ -1,7 +1,7 @@
 <?php
 for ($i=1; $i<=100; $i++) {
     if ($i % 3 && $i % 5) {
-        echo $i.' ';
+        echo $i;
     } else {
         if ($i % 3 == 0)
             echo 'Fizz';
