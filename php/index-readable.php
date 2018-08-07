cd042d2efe00bce446afd8cfa69f9411d44c4bfb
Pete Darling <pete@natter.ai>
2018-08-07 14:41:57 +0100

    More readable but still is PHP  code

---
+++ php/index-readable.php
@@ -0,0 +1,12 @@
+<?php
+for ($i=1; $i<=100; $i++) {
+    if ($i % 3 && $i % 5) {
+        echo $i.' ';
+    } else {
+        if ($i % 3 == 0)
+            echo 'Fizz';
+        if ($i % 5 == 0)
+            echo 'Buzz';
+    }
+    echo PHP_EOL;
+}
