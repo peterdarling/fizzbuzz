372775fab39b02b2004772412991df1fe5e5ef97
Pete Darling <pete@natter.ai>
2018-08-08 14:51:42 +0100

    Updated Haskell version

--- haskell/fizzbuzz.hs
+++ haskell/fizzbuzz.hs
@@ -1,5 +1,13 @@
 import System.IO
 
-main = do
-    putStrLn "TODO!"
+fizzBuzz :: Int -> String
+fizzBuzz x
+    | (x `mod` 15 == 0) = "Fizzbuzz"
+    | (x `mod` 3 == 0) = "Fizz"
+    | (x `mod` 5 == 0) = "Buzz"
+    | otherwise = show x
 
+fizzBuzz1To100 = map fizzBuzz [1..100]
+
+main = do
+    putStr $ unlines $ map fizzBuzz [1..100]
